464ee3dcf28fd805fca7a10ffeb341f4ffa8b3df
ryannd <ryan2dimaranan@gmail.com>
2023-12-05 13:54:16 -0600

    :tada: (structure) init project
    koa boilerplate and dev tools

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import Koa from "koa";
+import json from "koa-json";
+import Router from "koa-router";
+import logger from "koa-logger";
+import bodyParser from "koa-bodyparser";
+
+// import initRoutes from '~/routes'
+
+const app = new Koa();
+const router = new Router();
+
+const PORT = process.env.PORT || 8000;
+
+app.use(logger());
+app.use(json());
+app.use(bodyParser());
+app.use(router.routes()).use(router.allowedMethods());
+
+// initRoutes(router)
+
+app.listen(PORT, () => {
+    console.log("🚀 ~ Koa started and listening to", PORT);
+});
